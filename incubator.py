0317170b0d2f041cbd8e8dee48f88beb78886dba
Sota Kaneko <sotakaneko0808@gmail.com>
2018-12-17 23:09:11 +0900

    init

--- incubator.py
+++ incubator.py
@@ -1,1 +1,25 @@
+#Ver 0.2 dev 1
+import bme280
+import RPi.GPIO as GPIO
+import i2clcd
+from sys import exit
+import threading
+import time
+
+class hardware:
+    # Hardware pins are numbered in BCM
+    heatpad_pin = 19
+    up_switch_pin = 11
+    down_switch_pin = 13
+
+class internal_variables:
+    # target_temp here will determine the target on startup
+    target_temp = 37.0
+    # temp_offset is to eliminate any discrepancies between the sensor readings and actual readings in the box
+    temp_offset = 0.0
+    # temp and humidity are here just because
+    temp = 0.0
+    humidity = 0.0
+    # implement a kill signal for all looping threads
+    kill_thread = False
 
