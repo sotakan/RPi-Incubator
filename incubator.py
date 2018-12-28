d3f26472488ecfb9fb40125674d5983beda47a59
sotakan <sotakan@protonmail.com>
2018-12-28 09:53:16 +0000

    Updated incubator.py

--- incubator.py
+++ incubator.py
@@ -83,7 +83,7 @@ def ota():
 try:
     with open("temp.conf", "r+") as file:
         file = file.read()
-        print file
+        file = float(file)
         variables.target_temp = float(round(file, 2))
 except IOError:
     with open("temp.conf", "w") as file:
