0037efdccb6cf7a41469f7e12f41f9f4f346e4f1
Sota Kaneko <sotakaneko0808@gmail.com>
2019-05-28 09:01:56 +0900

    Update updater.sh

--- updater.sh
+++ updater.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Installing dependencies
+# Install dependencies
 sudo apt-get update && sudo apt-get install -y mysql.connector python-pycurl
 # Make new exec.sh executable
 sudo chmod a+x $PWD/exec.sh
